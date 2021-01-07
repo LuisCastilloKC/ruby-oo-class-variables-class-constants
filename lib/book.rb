30fe29538d99cd51c29b914526c24bfc62f0aceb
Luis Castillo <raul@luiss-mbp.myfiosgateway.com>
2021-01-06 19:25:54 -0500

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,13 +1,34 @@
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
+
+  GENRES = []
 
   def initialize(title)
     @title = title
+    GENRES << @genre
   end
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
 
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre
+  end
+
 end
+
+
+# Book
+#   GENRES
+#     keeps track of all genres
+
+# Shoe
+#   BRANDS
+#     keeps track of all brands
+#     only keeps track of unique brands
+
+# Finished in 0.00529 seconds (files took 0.11462 seconds to load)
+# 3 examples, 0 failures
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,29 @@
+
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
+    if BRANDS.include?(brand)
+    else
+      BRANDS << brand
   end
+end
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-
 end
+
+# Book
+#   GENRES
+#     keeps track of all genres
+
+# Shoe
+#   BRANDS
+#     keeps track of all brands
+#     only keeps track of unique brands
